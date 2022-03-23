afb6436c63dfde695d5984d4e0a8a65e2bd9286c
AurelienLouvel <louvel.aurelien.perso@gmail.com>
2022-03-23 09:51:07 +0100

    fix(accueil) update date names

--- screens/Concerts.js
+++ screens/Concerts.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { ScrollView, StyleSheet, Text, View, Dimensions } from "react-native";
+import { Colors, BackgroundColor } from "../theme/Colors";
+import TextStyles from "../theme/TextStyles";
+import { Padding } from "../theme/Spacing";
+import Map from "../components/Map";
 
 export default function Concerts() {
   return (
     <ScrollView>
-      <View>
-        <Text>Concerts</Text>
+      <View style={Styles.container}>
+        <Map />
       </View>
     </ScrollView>
   );
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
